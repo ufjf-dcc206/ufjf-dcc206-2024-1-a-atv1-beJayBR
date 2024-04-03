4c25cc26839da46afe782911db610a227c84d7f1
Breno Junqueira <breno.junqueira@estudante.ufjf.br>
2024-04-02 21:00:06 -0300

    Correcao de proximo.test.js

--- ufjf-dcc206-2024-1-a-atv1/proximo.test.js
+++ ufjf-dcc206-2024-1-a-atv1/proximo.test.js
@@ -1,24 +1,22 @@
-import {test,expect} from vitest
-import {proximo} from './proximo.js';
+import {test, expect} from 'vitest';
+import {proximo} from "./proximo.js"
 
-describe('proximo()', async () => {
-    test('proximo(0) deve ser 1' , () => {    
-    expect(proximo(0)).toBe(1);
-    });
+test("proximo(0) retorna 1", () => {
+    expect(proximo(0)).toBe(1)
+})
 
-    test('proximo(1) deve ser 3' , () => {    
-        expect(proximo(1)).toBe(3);
-        });
+test("proximo(1) retorna 3", () => {
+    expect(proximo(1)).toBe(3)
+})
 
-    test('proximo(2) deve ser 5' , () => {    
-        expect(proximo(2)).toBe(5);
-        });
+test("proximo(2) retorna 5", () => {
+    expect(proximo(2)).toBe(5)
+})
 
-    test('proximo(3) deve ser 4' , () => {    
-        expect(proximo(3)).toBe(4);
-        });
+test("proximo(3) retorna 4", () => {
+    expect(proximo(3)).toBe(4)
+})
 
-    test('proximo(4) deve ser 6' , () => {    
-        expect(proximo(4)).toBe(6);
-        });
+test("proximo(4) retorna 6", () => {
+    expect(proximo(4)).toBe(6)
 })
